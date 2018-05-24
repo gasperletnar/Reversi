708c71be70bd41c75a3b3a88adf696ffa67da420
Gasper Letnar <gasper.letnar@gmail.com>
2018-05-24 19:22:39 +0200

    Izboljsave.

--- ProjektReversi/src/inteligenca/Ocena.java
+++ ProjektReversi/src/inteligenca/Ocena.java
@@ -5,62 +5,31 @@ import logika.Igralec;
 import logika.Polje;
 
 public class Ocena {
-	// Vrednost zmage, ki mora biti veliko vecja kot vse ostale vrednosti
-	public static final int ZMAGA = (1 << 20);
-	// Vrednost izgube, ki mora biti veliko manjša kot vse ostale vrednosti
-	public static final int ZGUBA = -ZMAGA;
-	// Vrednost neodlocenega izida
-	public static final int NEODLOCENO = 0;
-	// Dolocimo vrednost polj v kotu, ki je pri reversiju zelo pomemben
-	public static final int VREDNOST_KOTA = 50;
-	// Vrednost sosedov kota je negativna, saj kadar imamo figure na teh pozicijah,
-	// lahko nasprotnik postavi figuro v kot
-	public static final int VREDNOST_SOSEDOV_KOTA = -10;
-	// Dolocimo vrednost polj na robu, ki sicer nisto pomembni kot kot, so pa bolj
-	// kot nerobna polja
-	public static final int VREDNOST_ROBA = 5;
-	public static final int VREDNOST_NEROBNEGA = 1;
-	public static final int VREDNOST_MOZNE_POTEZE = 2;
+	private static final int ZMAGA = (1 << 20);
+	private static final int ZGUBA = -ZMAGA;
+	private static final int NEODLOCENO = 0;
 	
-	private static int[][] pari = {{0,1}, {1,0}, {1,1}};
-	private static int n = Igra.N-1;
-	private static int[][] koti = {{0,0}, {n,0}, {0,n}, {n,n}};
-	private static int[][] sosediKotov = new int[12][];
-	static {
-		int i = 0;
-		for (int[] par : pari) {
-			for (int[] kot : koti) {
-				int a = (kot[0] == 0? kot[0] + par[0] : kot[0] - par[0]);
-				int b = (kot[1] == 0? kot[1] + par[1] : kot[1] - par[1]);
-				sosediKotov[i][0] = a;
-				sosediKotov[i][1] = b;
-				i++;
-			}
-		}
-	}
+	private static final int POLJE = 1;
+	private static final int KOT = 50;
+	private static final int ROBNI_SOSED = -10;
+	private static final int DIAGONALNI_SOSED = -20;
+	private static final int ROB = 5;
 	
-	public boolean aliJeKot(int a, int b) {
-		for (int[] kot : koti) {
-			if (a == kot[0] && b == kot[1]) {
-				return true;
-			}
-		}
-		return false;
-	}
+	private static final int N = Igra.N-1;
+	private static final int[][] KOTI = {{0,0}, {0,N}, {N,0}, {N,N}};
+	private static final int[][] ROBNI_SOSEDI = {{0,1}, {0,N-1}, {1,0}, {1,N}, {N,1}, {N,N-1}, {N-1,0}, {N-1,N}};
+	private static final int[][] DIAGONALNI_SOSEDI = {{1,1}, {1,N-1}, {N-1,1}, {N-1,N-1}};
 	
-	public boolean aliJeSosedKota(int a, int b) {
-		for (int[] sosedi : sosediKotov) {
-			if (a == sosedi[0] && b == sosedi[1]) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public boolean aliJeRob(int a, int b) {
-		return (a == 0 || b == 0 || a == n || b == n);
-	}
-	
+	/**
+	 * Oceni koliko je vredna trenutna pozicije igre za danega igralca. Vrednost ocene je podana glede na polja na plosci,
+	 * kjer se vrednosti ocene pristevajo/odstevajo glede na to, cigavo je polje in tip polja. Koti so vredni najvec, nato robovi
+	 * ter nazadnje ostala polja. Izjema so sosedi kotov, ki se stejejo negativno, saj so to slaba polja za igralca. Obratno, ce
+	 * ima nasprotni igralec neko polje, se vrednosti odstevajo(pristevajo v primeru sosedov kota).
+	 * 
+	 * @param jaz Igralec, katerega oceno zelimo dobiti.
+	 * @param igra Trenutno stanje igre. (ne spreminjaj tega objekta).
+	 * @return Ocena vrednosti pozicije za danega igralca(zagotovo pravilna, ce je igre konec).
+	 */
 	public static int oceniPozicijo(Igralec jaz, Igra igra){
 		switch(igra.stanje()){
 		case ZMAGA_BELI:
@@ -72,91 +41,187 @@ public class Ocena {
 		case NA_POTEZI_BELI:
 		case NA_POTEZI_CRNI:
 			Polje[][] plosca = igra.getPlosca();
-			int vrednostB = 0;
-			int vrednostC = 0;
-			// Preverimo, kateri igralec ima v kotih figuro.
-			// To kodo bi se dalo spravit v spodnjo for zanko, a bi potrebovali
-			// vec if stavkov
-			for (int k = 0; k<Igra.N; k = k+(Igra.N - 1)){
-				for (int h = 0; h<Igra.N; h = h+(Igra.N - 1)){
-					switch(plosca[k][h]){
-					case CRNO: vrednostB+=VREDNOST_KOTA;
-					case BELO: vrednostC+=VREDNOST_KOTA;
-					default: break;
+			int vrednostBeli = 0;
+			int vrednostCrni = 0;
+			
+			// Pregledamo notranje polja plosce in pristevamo vrednosti tistemu igralcu, cigar polje je.
+			for (int i = 1; i < N; i++) {
+				for (int j = 1; j < N; j++) {
+					switch (plosca[i][j]) {
+					case BELO: vrednostBeli += POLJE; break;
+					case CRNO: vrednostCrni += POLJE; break;
+					case PRAZNO: break;
 					}
 				}
 			}
-			// Preverimo, ali so figure na mestih, ki so sosednji kotom.
-			// Podobno, zaradi lazjega zapisa in manj if stavkov, se ta koda piše
-			// izven spodnje glavne for zanke
-			for (int a = 0; a<=1; a++){
-				for (int b = 0; b<=1; b++){
-					// Koda se na tak nacin zelo skrajša, ni potrebno naredit
-					// treh for zank.
-					if (a != 0 && b != 0){
-						for (int k = a; k<Igra.N; k = k+(Igra.N - (1 + 2*a))){
-							for (int h = b; h<Igra.N; h = h+(Igra.N - (1 + 2*b))){
-								switch(plosca[k][h]){
-								case CRNO: vrednostB+=VREDNOST_SOSEDOV_KOTA;
-								case BELO: vrednostC+=VREDNOST_SOSEDOV_KOTA;
-								default: break;
-								}
-							}
-						}
-					}
+			
+			// Kot prej, tukaj za kote.
+			for (int[] par : KOTI) {
+				switch (plosca[par[0]][par[1]]) {
+				case BELO: vrednostBeli += KOT; break;
+				case CRNO: vrednostCrni += KOT; break;
+				case PRAZNO: break;
 				}
-				
 			}
-			// Dolociti je treba vrednost po razlicnih kriterijih, sprehodimo 
-			// se po vseh poljih
-			for (int i = 0; i < Igra.N; i++) {
-				for (int j = 0; j < Igra.N; j++) {
-					// Dodelimo igralcu vrednost roba, ce ima polje na robu,
-					// ne smemo steti sosedov roba
-					// Sprva po horizontalni smeri
-					if (i == 0 || i == (Igra.N - 1)){
-						if (!(j < 2) && !(j > (Igra.N - 2))){
-							switch(plosca[i][j]){
-							case CRNO: vrednostB+=VREDNOST_ROBA;
-							case BELO: vrednostC+=VREDNOST_ROBA;
-							default: break;
-							}
-						}
-					}
-					else{
-						// Se vertikalna smer
-						if (j == 0 || j == (Igra.N - 1)){
-							if (!(i < 2) && !(i > (Igra.N - 2))){
-								switch(plosca[i][j]){
-								case CRNO: vrednostB+=VREDNOST_ROBA;
-								case BELO: vrednostC+=VREDNOST_ROBA;
-								default: break;
-								}
-							}
-						}
-						else{
-							// Vsi preostali bodo za zdaj imeli vrednost 1
-							switch(plosca[i][j]){
-							case CRNO: vrednostB+=VREDNOST_NEROBNEGA;
-							case BELO: vrednostC+=VREDNOST_NEROBNEGA;
-							default: break;
-							}
-						}
+			
+			// Robni sosedi - vsak kot ima 3 sosede, 2 robna enega diagonalnega.
+			for (int[] par : ROBNI_SOSEDI) {
+				switch (plosca[par[0]][par[1]]) {
+				case BELO: vrednostBeli += ROBNI_SOSED; break;
+				case CRNO: vrednostCrni += ROBNI_SOSED; break;
+				case PRAZNO: break;
+				}
+			}
+			
+			// Tukaj potrebno v primeru da pristejemo vrednost enemu od igralcev, da leto zmanjsamo za vrednost POLJE,
+			// saj smo ga steli ze ko smo sli cez notranjost plosce.
+			for (int[] par : DIAGONALNI_SOSEDI) {
+				switch (plosca[par[0]][par[1]]) {
+				case BELO: vrednostBeli += DIAGONALNI_SOSED - POLJE; break;
+				case CRNO: vrednostCrni += DIAGONALNI_SOSED - POLJE; break;
+				case PRAZNO: break;
+				}
+			}
+			
+			// Robna polja. Simetrija - v tabeli robna so 4 simetricna polja.
+			for (int i = 2; i < N-1; i++) {
+				Polje[] robna = {plosca[i][0], plosca[i][N], plosca[0][i], plosca[N][i]};
+				for (Polje polje : robna) {
+					switch (polje) {
+					case BELO: vrednostBeli += ROB; break;
+					case CRNO: vrednostCrni += ROB; break;
+					case PRAZNO: break;
 					}
 				}
 			}
-			// Sedaj bomo dodelili se vrednost, glede na to, koliko ima igralec
-			// moznih potez
-			if (jaz == Igralec.BELI){
-				vrednostB+=igra.seznamDovoljenih().size() * VREDNOST_MOZNE_POTEZE;
-			}
-			else{
-				vrednostC+=igra.seznamDovoljenih().size() * VREDNOST_MOZNE_POTEZE;
-			}
-			return (jaz == Igralec.CRNI ? (vrednostC - vrednostB) : (vrednostB - vrednostC));
+			return (jaz == Igralec.CRNI ? (vrednostCrni - vrednostBeli) : (vrednostBeli - vrednostCrni));
 		}
-		return 42;
+		assert false;
+		return 9;
 	}
+	
+	
+	
+//	private static void pristej(Igra igra, int i, int j, int beli, int crni, int vrednost) {
+//	Polje[][] plosca = igra.getPlosca();
+//	switch (plosca[i][j]) {
+//	case BELO: beli += vrednost; break;
+//	case CRNO: crni += vrednost; break;
+//	case PRAZNO: break;
+//	}
+//}
+	
+//	private static int[][] koti = {{0,0}, {,0}, {0,}, {,}};
+//	private static int[][] sosediKotov = new int[12][];
+//	static {
+//		int i = 0;
+//		for (int[] par : pari) {
+//			for (int[] kot : koti) {
+//				int a = (kot[0] == 0? kot[0] + par[0] : kot[0] - par[0]);
+//				int b = (kot[1] == 0? kot[1] + par[1] : kot[1] - par[1]);
+//				sosediKotov[i][0] = a;
+//				sosediKotov[i][1] = b;
+//				i++;
+//			}
+//		}
+//	}
+	
+//	public static int oceniPozicijo(Igralec jaz, Igra igra){
+//		switch(igra.stanje()){
+//		case ZMAGA_BELI:
+//			return (jaz == Igralec.BELI ? ZMAGA : ZGUBA);
+//		case ZMAGA_CRNI:
+//			return (jaz == Igralec.CRNI ? ZMAGA : ZGUBA);
+//		case NEODLOCENO:
+//			return NEODLOCENO;
+//		case NA_POTEZI_BELI:
+//		case NA_POTEZI_CRNI:
+//			Polje[][] plosca = igra.getPlosca();
+//			int vrednostB = 0;
+//			int vrednostC = 0;
+//			// Preverimo, kateri igralec ima v kotih figuro.
+//			// To kodo bi se dalo spravit v spodnjo for zanko, a bi potrebovali
+//			// vec if stavkov
+//			for (int k = 0; k<Igra.N; k = k+(Igra.N - 1)){
+//				for (int h = 0; h<Igra.N; h = h+(Igra.N - 1)){
+//					switch(plosca[k][h]){
+//					case CRNO: vrednostB+=VREDNOST_KOTA;
+//					case BELO: vrednostC+=VREDNOST_KOTA;
+//					default: break;
+//					}
+//				}
+//			}
+//			// Preverimo, ali so figure na mestih, ki so sosednji kotom.
+//			// Podobno, zaradi lazjega zapisa in manj if stavkov, se ta koda piše
+//			// izven spodnje glavne for zanke
+//			for (int a = 0; a<=1; a++){
+//				for (int b = 0; b<=1; b++){
+//					// Koda se na tak nacin zelo skrajša, ni potrebno naredit
+//					// treh for zank.
+//					if (a != 0 && b != 0){
+//						for (int k = a; k<Igra.N; k = k+(Igra.N - (1 + 2*a))){
+//							for (int h = b; h<Igra.N; h = h+(Igra.N - (1 + 2*b))){
+//								switch(plosca[k][h]){
+//								case CRNO: vrednostB+=VREDNOST_SOSEDOV_KOTA;
+//								case BELO: vrednostC+=VREDNOST_SOSEDOV_KOTA;
+//								default: break;
+//								}
+//							}
+//						}
+//					}
+//				}
+//				
+//			}
+//			// Dolociti je treba vrednost po razlicnih kriterijih, sprehodimo 
+//			// se po vseh poljih
+//			for (int i = 0; i < Igra.N; i++) {
+//				for (int j = 0; j < Igra.N; j++) {
+//					// Dodelimo igralcu vrednost roba, ce ima polje na robu,
+//					// ne smemo steti sosedov roba
+//					// Sprva po horizontalni smeri
+//					if (i == 0 || i == (Igra.N - 1)){
+//						if (!(j < 2) && !(j > (Igra.N - 2))){
+//							switch(plosca[i][j]){
+//							case CRNO: vrednostB+=VREDNOST_ROBA;
+//							case BELO: vrednostC+=VREDNOST_ROBA;
+//							default: break;
+//							}
+//						}
+//					}
+//					else{
+//						// Se vertikalna smer
+//						if (j == 0 || j == (Igra.N - 1)){
+//							if (!(i < 2) && !(i > (Igra.N - 2))){
+//								switch(plosca[i][j]){
+//								case CRNO: vrednostB+=VREDNOST_ROBA;
+//								case BELO: vrednostC+=VREDNOST_ROBA;
+//								default: break;
+//								}
+//							}
+//						}
+//						else{
+//							// Vsi preostali bodo za zdaj imeli vrednost 1
+//							switch(plosca[i][j]){
+//							case CRNO: vrednostB+=VREDNOST_NEROBNEGA;
+//							case BELO: vrednostC+=VREDNOST_NEROBNEGA;
+//							default: break;
+//							}
+//						}
+//					}
+//				}
+//			}
+//			// Sedaj bomo dodelili se vrednost, glede na to, koliko ima igralec
+//			// moznih potez
+//			if (jaz == Igralec.BELI){
+//				vrednostB+=igra.seznamDovoljenih().size() * VREDNOST_MOZNE_POTEZE;
+//			}
+//			else{
+//				vrednostC+=igra.seznamDovoljenih().size() * VREDNOST_MOZNE_POTEZE;
+//			}
+//			return (jaz == Igralec.CRNI ? (vrednostC - vrednostB) : (vrednostB - vrednostC));
+//		}
+//		return 42;
+//	}
 }
 
 
